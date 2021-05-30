f28e8e06237dd1e58b3cd50f57ecedaa625f6894
KusumaHN <kusumahn23@gmail.com>
2021-05-31 03:55:53 +0530

    First

---
+++ src/com/java/kushi/Javabasic.java
@@ -0,0 +1,11 @@
+package com.java.kushi;
+
+public class Javabasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Kusuma");
+	}
+
+}
---
+++ src/com/java/kushi/print1to10.java
@@ -0,0 +1,15 @@
+package com.java.kushi;
+
+public class print1to10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=0; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
